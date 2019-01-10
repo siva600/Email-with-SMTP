2afc969a0c4f35921fa601a764d2f74c29782978
Siva. V <9643073+siva600@users.noreply.github.com>
2019-01-09 17:06:00 -0800

    Add files via upload

---
+++ test_email.py
@@ -0,0 +1,38 @@
+import smtplib
+import email.message
+
+
+def html_content():
+    read_html_content = open('message.html')
+    data = read_html_content.read()
+    return data
+
+
+def main():
+    try:
+        msg = email.message.Message()
+        msg['Subject'] = 'SMTP Test email'
+
+        # Login credentials
+        msg['From'] = 'email'
+        msg['To'] = 'email'
+        password = "password of From email"
+
+        msg.add_header('Content-Type', 'text/html')
+        msg.set_payload(html_content())
+
+        # create server
+        s = smtplib.SMTP('smtp.gmail.com: 587')
+        s.starttls()
+        s.login(msg['From'], password)
+
+        # sending message via the server
+        s.sendmail(msg['From'], [msg['To']], msg.as_string())
+        print("Email sent")
+        s.quit()
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    main()
